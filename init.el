225063812f691759436205a79c9d90dd98005081
u9855 <u9855@users.noreply.github.com>
2017-06-25 22:06:15 +0900

    init.el: Set the priority of the character sets

--- init.el
+++ init.el
@@ -14,6 +14,9 @@
 (prefer-coding-system
  (if (eq system-type 'windows-nt) 'cp932-dos 'utf-8-unix))
 (set-coding-system-priority 'utf-8 'euc-jp 'iso-2022-jp 'cp932)
+
+(set-charset-priority 'ascii 'japanese-jisx0208 'latin-jisx0201
+                      'katakana-jisx0201 'iso-8859-1 'cp1252 'unicode)
 
 ;; Keymaps
 ;; -------
