2f533c306f8fcdad0a1e8599fb0d1ac77284879f
u9855 <u9855@users.noreply.github.com>
2017-06-25 21:46:30 +0900

    init.el: Add the comment about the requirements

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
 ;;; init.el --- The initialization file for GNU Emacs
 
+;;; Commentary:
+
+;; The file requires GNU Emacs version 25.1 or later.
+
 ;; Local Variables:
 ;; coding: utf-8-unix
 ;; indent-tabs-mode: nil
