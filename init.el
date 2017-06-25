a8f90ea1b460236da3bdf8891136944f05bd3425
u9855 <u9855@users.noreply.github.com>
2017-06-25 23:25:20 +0900

    init.el: Set to display a file name or a buffer name in the title bar

--- init.el
+++ init.el
@@ -70,6 +70,8 @@
  '(initial-frame-alist
    '((height . 43) (width . 100) (font . "Consolas-11")))
  '(default-frame-alist initial-frame-alist)
+ '(frame-title-format
+   '("Emacs@" (:eval (system-name)) " - " (buffer-file-name "%f" "%b")))
  '(cursor-in-non-selected-windows nil)
  '(indicate-buffer-boundaries '((top . left) (bottom . left)))
  '(menu-bar-mode (display-graphic-p))
