933e16d45d97345de7c4e2072cc78ee58e2b02ff
u9855 <u9855@users.noreply.github.com>
2017-06-25 22:10:52 +0900

    init.el: Modify charsets of Japanese locale in cjk-char-width-table-list

--- init.el
+++ init.el
@@ -17,6 +17,33 @@
 
 (set-charset-priority 'ascii 'japanese-jisx0208 'latin-jisx0201
                       'katakana-jisx0201 'iso-8859-1 'cp1252 'unicode)
+
+(let ((table (assq 'ja_JP cjk-char-width-table-list)))
+  (when table
+    (setcdr table
+            '(nil
+              ;; JIS X 0208 の文字集合は JIS X 2013 で規定されている。
+              (japanese-jisx0213.2004-1 (#x2121 . #x2d7e))
+              (japanese-jisx0212        (#x2121 . #x2b7e))
+              (cp932-2-byte             (#x8140 . #x879f))
+              (unicode                  (#x02c4 . #x02c4) (#x02c9 . #x02cb)
+                                        (#x02cd . #x02cd) (#x02df . #x02df)
+                                        (#x2024 . #x2027) (#x2035 . #x2035)
+                                        (#x203e . #x203e) (#x2074 . #x2074)
+                                        (#x207f . #x207f) (#x2081 . #x2084)
+                                        (#x2105 . #x2105) (#x2109 . #x2109)
+                                        (#x2126 . #x2126) (#x215b . #x215e)
+                                        (#x21b8 . #x21b9) (#x220f . #x220f)
+                                        (#x2215 . #x2215) (#x2223 . #x2223)
+                                        (#x224c . #x224c) (#x226e . #x226f)
+                                        (#x2299 . #x2299) (#x2550 . #x2573)
+                                        (#x2580 . #x258f) (#x2592 . #x2595)
+                                        (#x25a3 . #x25a9) (#x25e2 . #x25e5)
+                                        (#x2609 . #x2609) (#x2614 . #x2615)
+                                        (#x261c . #x261c) (#x273d . #x273d)
+                                        (#xe000 . #xf8ff) (#xfe00 . #xfe0f)
+                                        (#xfffd . #xfffd))))
+    (use-cjk-char-width-table 'ja_JP)))
 
 ;; Keymaps
 ;; -------
