94e026ab54de7de1c3a806bbe484642067be2177
u9855 <u9855@users.noreply.github.com>
2017-07-02 14:24:16 +0900

    init.el: Use auto-save-buffers-enhanced

--- init.el
+++ init.el
@@ -164,6 +164,8 @@ non-nil に設定されているとインストールに失敗するので一時
 
   (dash-enable-font-lock))
 
+(use-package auto-save-buffers-enhanced)
+
 (use-package cp5022x
   :config (define-coding-system-alias 'euc-jp 'cp51932))
 
