4eebd5cf8b9b734307ca743cea89e75a960e4422
u9855 <u9855@users.noreply.github.com>
2017-07-03 22:07:16 +0900

    init.el: Remove the unnecessary commands from easy-repeat-command-list

--- init.el
+++ init.el
@@ -234,7 +234,13 @@ non-nil に設定されているとインストールに失敗するので一時
                      (t dabbrev-abbrev-char-regexp))))))
       (funcall fn arg))))
 
-(use-package easy-repeat)
+(use-package easy-repeat
+  :config
+  (custom-set-variables
+   '(easy-repeat-command-list
+     (-difference easy-repeat-command-list
+                  '(recenter-top-bottom kill-buffer scroll-up-command
+                    scroll-down-command beginning-of-defun end-of-defun)))))
 
 (use-package ffap
   :config
