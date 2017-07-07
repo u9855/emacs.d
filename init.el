b5d0d7105a12c625482c160752b65a700be126b4
u9855 <u9855@users.noreply.github.com>
2017-07-07 22:20:47 +0900

    init.el: Set the maximum number of recent files that will be saved to 100

--- init.el
+++ init.el
@@ -268,7 +268,10 @@ non-nil に設定されているとインストールに失敗するので一時
   :config (custom-set-variables
            '(log-edit-hook '(log-edit-insert-filenames-without-changelog))))
 
-(use-package recentf)
+(use-package recentf
+  :config
+  (custom-set-variables
+   '(recentf-max-saved-items 100)))
 
 (use-package solarized-theme
   :config
