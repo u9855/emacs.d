061d2c7dc38ef943a3b227ae42db4a43072315f5
u9855 <u9855@users.noreply.github.com>
2017-07-29 10:30:58 +0900

    init.el: Change the value of auto-save-buffers-enhanced-interval to 1

--- init.el
+++ init.el
@@ -201,7 +201,7 @@ non-nil に設定されているとインストールに失敗するので一時
 (use-package auto-save-buffers-enhanced
   :config
   (custom-set-variables
-   '(auto-save-buffers-enhanced-interval 2)
+   '(auto-save-buffers-enhanced-interval 1)
    '(auto-save-buffers-enhanced-include-regexps nil))
 
   (define-advice auto-save-buffers-enhanced-save-buffers
