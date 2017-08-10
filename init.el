054bbf4bbe8dbea9f99400497b869c51532f6b0d
u9855 <u9855@users.noreply.github.com>
2017-08-10 21:57:26 +0900

    init.el: Change the font for multibyte characters to MeiryoKe_Console

--- init.el
+++ init.el
@@ -139,8 +139,8 @@
 
 (dolist
     (target '(japanese-jisx0212 japanese-jisx0213.2004-1 katakana-jisx0201))
-  (set-fontset-font t target (font-spec :family "Meiryo UI")))
-(set-fontset-font t 'unicode (font-spec :family "Meiryo UI") nil 'append)
+  (set-fontset-font t target (font-spec :family "MeiryoKe_Console")))
+(set-fontset-font t 'unicode (font-spec :family "MeiryoKe_Console") nil 'append)
 
 (dir-locals-set-class-variables
  'shared-library
