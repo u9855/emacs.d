022af4de5133b675c277ddeb360cb43c45e17efe
u9855 <u9855@users.noreply.github.com>
2017-09-21 22:55:40 +0900

    init.el: Set ffap to guess local filenames at point in helm-find-files

--- init.el
+++ init.el
@@ -459,7 +459,8 @@ non-nil に設定されているとインストールに失敗するので一時
    '(helm-M-x-requires-pattern 2)
    '(helm-command-prefix-key "C-c c")
    '(helm-ff-fuzzy-matching nil)
-   '(helm-ff-skip-boring-files t))
+   '(helm-ff-skip-boring-files t)
+   '(helm-ff-guess-ffap-filenames t))
 
    (with-eval-after-load 'helm-buffers
      (custom-set-variables
