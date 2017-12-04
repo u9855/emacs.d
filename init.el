8153d0852045fd8deb0c8a87af697d5d6b9e2eb0
u9855 <u9855@users.noreply.github.com>
2017-12-04 22:36:30 +0900

    init.el: Set flycheck-keymap-prefix to the 'C-c `' key

--- init.el
+++ init.el
@@ -512,7 +512,8 @@ non-nil に設定されているとインストールに失敗するので一時
    '(find-ls-option '("-exec ls -Fdhlv {} +" . "-Fdhlv"))
    '(find-ls-subdir-switches dired-listing-switches)))
 
-(use-package flycheck)
+(use-package flycheck
+  :init (custom-set-variables '(flycheck-keymap-prefix (kbd "C-c `"))))
 
 (use-package goto-chg
   :bind (:map goto-map
